cf492bad2f197feed1a60a84fb9b9a52b2116188
KongProgammer <81782546+KongProgammer@users.noreply.github.com>
2022-01-09 12:56:57 -0300

    Ajeitando Bug do buycar

--- src/commands/Economia/buycar.js
+++ src/commands/Economia/buycar.js
@@ -9,20 +9,12 @@ exports.run = async (bot, message, args) => {
         let gargemcarro= await db.fetch(message.guild.id, { gargemcarro: [] });
         let amount = await db.fetch(`social_${message.guild.id}_${message.author.id}`)
 // items
-        if(purchase === 'Chevrolet-Astra-2.0-Hatch'){
+if(purchase === 'Chevrolet-Astra-2.0-Hatch'){
      
-            if(amount < 30000) return message.channel.send('você não possui dinheiro suficiente para realizar a compra!');
-            db.subtract(`social_${message.guild.id}_${message.author.id}`, 30000);
-            db.push(message.author.id, "Chevrolet-Astra-2.0-Hatch");
-           const embed1 = new Discord.MessageEmbed()
-.setColor('#00004d')
-.setDescription(`
-✅Sucesso
-Seu novo carro Chevrolet-Astra-2.0-Hatch ja ta na garagem.
-`)
-.setImage("https://cdn.discordapp.com/attachments/929437812980330526/929761099086184540/1641742653553.png")
-
- message.channel.send(embed1);
+    if(amount < 30000) return message.channel.send('você não possui dinheiro suficiente para realizar a compra!');
+    db.subtract(`social_${message.guild.id}_${message.author.id}`, 30000);
+    db.push(message.guild.id, "Chevrolet-Astra-2.0-Hatch");
+   message.channel.send("parabens voce comprou");
 
 
         }
