1bdeb64a7c0f9190b241c73b89d11f8a63845571
KongProgammer <81782546+KongProgammer@users.noreply.github.com>
2022-01-10 20:00:40 -0300

    criando o sistema, que acha os comandos Dentro  da pasta do comandos

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,4 +1,6 @@
-const { Client } =  require("discord.js")
+const { Client, PartialWebhookMixin } =  require("discord.js")
+const { readdirSync } = require("fs")
+const { join } = require("path")
 
 
 module.exports =  class extends Client {
@@ -6,5 +8,25 @@ module.exports =  class extends Client {
     constructor(options) {
 
         super(options)
+        this.commands = []
+
     }
+
+    loadCommands(path = 'src/commands') {
+        const categories =  readdirSync(path)
+
+        for (const category of categories) {
+            const commands  = readdirSync(`${path}/${category}`)
+
+            for (const command of categories) {
+                const commandClass = require(join(process.cwd(), `${path}/${category}/${command}`))
+                const cmd = new(commandClass)(this)
+
+
+                this.commands.push(cmd)
+                console.log(`commando ${cmd.name} carregado!`)
+            }
+        }
+    }
+
 }
